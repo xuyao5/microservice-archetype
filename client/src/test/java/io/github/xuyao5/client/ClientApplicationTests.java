1b9d99f8e4f62873096159ae7b1548ccd5a4c450
Thomas.XU <xuyao5@gmail.com>
2020-01-12 04:22:10 +0800

    add modules

---
+++ client/src/test/java/io/github/xuyao5/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xuyao5.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ service/src/test/java/io/github/xuyao5/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xuyao5.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
